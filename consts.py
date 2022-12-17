6a587ebcdbb65bedf37935953d468774fbd923ce
tomsloj <tomsloj@gmail.com>
2022-12-17 12:14:39 +0100

    add JITs addresses

--- consts.py
+++ consts.py
@@ -11,4 +11,14 @@ COMMON_PASSWORD = "mlodzi-gniewni"
 BIN2_JIT = "bin2@jix.im"
 COMMON_PASSWORD = "mlodzi-gniewni"
 
+TRUCK1_JIT = "truck1@jix.im"
+COMMON_PASSWORD = "mlodzi-gniewni"
+
+TRUCK2_JIT = "truck2@jix.im"
+COMMON_PASSWORD = "mlodzi-gniewni"
+
+LANDFILL_JIT = "landfill1@jix.im"
+COMMON_PASSWORD = "mlodzi-gniewni"
+
+
 BIN_INFORM_PERIOD = 10
